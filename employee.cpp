ced4ffb6694246099f4c87421d336a9f43c4ce10
jzcool <jzcool666@gmail.com>
2025-02-25 11:12:23 +0800

    完成了加入部门的操作，以及将删除函数的错误更改了

--- employee.cpp
+++ employee.cpp
@@ -13,16 +13,34 @@ struct employee* create_employee()
 	Employee* new_employee = (Employee*)malloc(sizeof(Employee));
 	if (!new_employee)
 	{
-		printf("创建员工失败！");
+		printf("创建员工失败！错误码2");
 		//此处有一个退回到上一个界面的函数
+		return NULL;
 	}
 	if (list_employee == NULL)
 	{
 		list_employee->next = new_employee;
 		list_employee->prev = NULL;
 		new_employee->prev = list_employee;
+		new_employee->next = NULL;
 	}
 	return new_employee;
+}
+struct department* create_department()
+{
+	Department* new_department = (Department*)malloc(sizeof(Department));
+	if (!new_department)
+	{
+		printf("创建部门失败！错误码3");
+		//此处也有一个返回到上一个界面的函数
+		return NULL;
+	}
+	if (list_department == NULL)
+	{
+		list_department->next = new_department;
+		new_department ->next = NULL;
+	}
+	return new_department;
 }
 void add_employee(Employee* last_employee)//添加员工的函数
 {
@@ -34,7 +52,7 @@ void add_employee(Employee* last_employee)//添加员工的函数
 	scanf("%s", new_employee->name);
 	
 	printf("请输入工号：\n");
-	scanf("%d", new_employee->number);
+	scanf("%d", &new_employee->number);
 	
 	printf("请输入职位：\n");
 	scanf("%s", new_employee->duty);
@@ -46,18 +64,56 @@ void add_employee(Employee* last_employee)//添加员工的函数
 	scanf("%s", new_employee->hometown);
 	
 	printf("请输入薪水：\n");
-	scanf("%d", new_employee->salary);
+	scanf("%d", &new_employee->salary);
 	
 	last_employee->next = new_employee;//双向链表，让前一个的后缀指向当前的
 	new_employee->prev = last_employee;//让后一个的前缀指向前一个
 	new_employee->next = NULL;//尾插法，后面是空的
-	total_last_employee = new_employee;
+	total_tail = new_employee;
+
+	//以上部分是将员工加入到总链表中，下面是将员工加入到部门链表当中
+	Department* head = list_department;
+	while (strcmp(head->name, new_employee->department) != 0&&head->next!=NULL)
+	{
+		head = head->next;
+	}
+	if (head->next == NULL)
+	{
+		head->next = create_department();
+		head = head->next;
+		strcpy(head->name, new_employee->department);
+	}
+	else
+	{
+		head->employee->prev = new_employee;
+		new_employee->next = head->employee;
+		new_employee->prev = NULL;
+		head->employee = new_employee;
+	}
+
 }
 
 void delete_employee(Employee* employee)//删除操作，这里可能需要先查找到员工后再删除，后续改进
 {
-	employee->prev = employee->next;
-	free(employee);
+		if (employee->prev != NULL)//删除头结点的特殊情况
+		{
+			employee->prev->next = employee->next;
+		}
+		else
+		{
+			list_employee = employee->next;
+		}
+
+		if (employee->next != NULL)//删除尾节点的特殊情况
+		{
+			employee->next->prev = employee->prev;
+		}
+		else
+		{
+			total_tail = employee->prev;
+		}
+
+		free(employee);
 }
 
 Employee* find_employee(int flag)//flag=1是姓名查找，flag=2是工号查找
--- employee.h
+++ employee.h
@@ -21,15 +21,17 @@ typedef struct department//部门的数据结构定义
 
 extern Employee* list_employee;//在employee.cpp中使用
 extern Department* list_department;//同上
-extern Employee* total_last_employee;//在所有员工当中最新加入的的那个
+extern Employee* total_tail;//在所有员工当中的尾节点
 
-void add_employee(Employee* last_employee);
+void add_employee(Employee* last_employee);//添加员工函数
 
-struct employee* create_employee();
+struct employee* create_employee();//创造员工函数
 
-Employee* find_employee(int flag);
+Employee* find_employee(int flag);//查询员工函数
 //这里的flag需要在图形界面处传递，若用户选择姓名查询则flag为1，反之为2，错误码为1时在此函数查看原因
 
-void delete_employee(Employee* employee)；
+void delete_employee(Employee* employee);//删除员工函数
+
+struct department* create_department();//创建部门函数
 
 #endif
