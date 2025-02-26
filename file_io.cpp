07dcda640f4e7ce671732e9073863d7e086b2bf1
jzcool <jzcool666@gmail.com>
2025-02-26 16:46:09 +0800

    增加了文件的读写功能

---
+++ file_io.cpp
@@ -0,0 +1,73 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+#include "employee.h"
+#include "department.h"
+#include "file_io.h"
+
+void save_file(const char* filename,Employee* employee)
+{
+	FILE* file = fopen(filename, "wb");
+
+	if (file == NULL)
+	{
+		perror("打开文件失败");
+		return;
+	}
+	
+	Data* temp = (Data*)malloc(sizeof(Data));
+	
+	strcpy(temp->name, employee->name);
+	
+	temp->number = employee->number;
+	
+	strcpy(temp->department, employee->department);
+
+	strcpy(temp->duty, employee->duty);
+
+	strcpy(temp->ID, employee->ID);
+
+	strcpy(temp->hometown, employee->hometown);
+
+	temp->salary = employee->salary;
+
+	fwrite(temp, sizeof(temp), 1, file);
+
+	fclose(file);
+		
+}
+
+Employee* load_file (const char* filename, Employee* employee)
+{
+	FILE* file = fopen(filename, "rb");
+
+	if (file == NULL)
+	{
+		perror("文件打开失败");
+		return;
+	}
+	
+	Data* temp = (Data*)malloc(sizeof(Data));
+
+	fread(temp, sizeof(Data), 1, file);
+
+	strcpy(employee->name, temp->name);
+
+	employee->number = temp->number;
+
+	strcpy(employee->department, temp->department);
+
+	strcpy(employee->duty, temp->duty);
+
+	strcpy(employee->ID, temp->ID);
+
+	strcpy(employee->hometown, temp->hometown);
+
+	employee->salary = temp->salary;
+
+	fclose(file);
+
+	return employee;
+
+}
--- file_io.h
+++ file_io.h
@@ -7,6 +7,20 @@ management是管理层部门的
 market是市场部的
 develop是开发部的
 finance是财务部的*/
+typedef struct temp_data
+{
+	char name[1024];
+	int	number;
+	char duty[1024];
+	char ID[1024];
+	int salary;
+	char hometown[1024];
+	char department[1024];
+}Data;
+
+void save_file(const char* filename, Employee* employee);//保存数据
+
+Employee* load_file(const char* filename, Employee* employee);//加载数据
 
 
 #endif FILE_IO_H
